ea553ed2aa9793e8718850e8fe90c485fb36d7f4
Rohith kumar R <rohithkumarrakasy@gmail.com>
2026-05-09 20:49:31 -0700

    feat: Module exports concept is impplemented

---
+++ Test.js
@@ -0,0 +1,9 @@
+const PersonDetails = require('./BasicClass');
+
+let age= ()=> console.log("Wanaparthy");
+
+console.log(age());
+
+let person= new PersonDetails("Naveen","Amaragonda");
+
+console.log(person.fullNameOfPerson());
