7bc8e0680ac0c2d4bc6b7da5acaa3df77434b6be
Rohith kumar R <rohithkumarrakasy@gmail.com>
2026-05-10 17:26:37 -0700

    fix: New code is implemented

--- BasicInheritance.js
+++ BasicInheritance.js
@@ -1,1 +1,19 @@
 /* Write code for Inheritance */
+
+const PersonDetails = require("./BasicClass");
+
+class Pet extends PersonDetails{
+
+    constructor(firstName,lastName){
+        super(firstName,lastName);        
+    }
+
+    get location(){
+        return "PetsMart";
+    }
+}
+
+let pet = new Pet("Sam","Doggy");
+console.log(pet.fullName);
+console.log(pet.location);
+console.log(pet.fullNameOfPerson());
